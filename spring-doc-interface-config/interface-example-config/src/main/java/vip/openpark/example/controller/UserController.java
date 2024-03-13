c5f15ba1b4f35a73aa66828a0501cb708954f695
anthony <anthony1949@zto.com>
2024-03-13 10:25:56 +0800

    feature: spring doc 接口配置请求方法和路径

--- spring-doc-interface-config/interface-example-config/src/main/java/vip/openpark/example/controller/UserController.java
+++ spring-doc-interface-config/interface-example-config/src/main/java/vip/openpark/example/controller/UserController.java
@@ -1,12 +1,11 @@
 package vip.openpark.example.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import vip.openpark.example.domain.request.UserInsertRequest;
 import vip.openpark.example.domain.request.UserUpdateRequest;
 import vip.openpark.example.domain.response.UserResponse;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -17,23 +16,28 @@ import java.util.List;
 @RequestMapping("user")
 public class UserController {
 	
+	@PostMapping
 	public String insert(@RequestBody UserInsertRequest userInsertRequest) {
 		return "success";
 	}
 	
+	@PutMapping
 	public String update(@RequestBody UserUpdateRequest userUpdateRequest) {
 		return "success";
 	}
 	
+	@DeleteMapping
 	public String delete(String id) {
 		return "success";
 	}
 	
-	public UserResponse get(String id) {
+	@GetMapping("{id}")
+	public UserResponse get(@PathVariable(name = "id") String id) {
 		return null;
 	}
 	
+	@GetMapping
 	public List<UserResponse> list(String id) {
-		return null;
+		return Collections.emptyList();
 	}
 }
